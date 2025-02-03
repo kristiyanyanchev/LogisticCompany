8976d214e0718bf47caf2616cf94c8d88566d968
Kristiyan Yanchev <kristiyan.yancheff@gmail.com>
2025-02-03 22:31:33 +0200

    added office crud

--- LogisticCompany/src/main/java/org/logistic/company/logisticcompany/persistance/service/UserService.java
+++ LogisticCompany/src/main/java/org/logistic/company/logisticcompany/persistance/service/UserService.java
@@ -1,20 +1,26 @@
 package org.logistic.company.logisticcompany.persistance.service;
 
+import org.logistic.company.logisticcompany.persistance.models.Authority;
 import org.logistic.company.logisticcompany.persistance.models.User;
+import org.logistic.company.logisticcompany.persistance.repos.AuthoritiesRepository;
+import org.logistic.company.logisticcompany.persistance.repos.AuthorityPK;
 import org.logistic.company.logisticcompany.persistance.repos.UserRepository;
 import org.logistic.company.logisticcompany.persistance.service.dto.UserDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
     private final OfficeService officeService;
+    private final AuthoritiesRepository authoritiesRepository;
 
-    public UserService(UserRepository userRepository, OfficeService officeService) {
+    public UserService(UserRepository userRepository, OfficeService officeService, AuthoritiesRepository authoritiesRepository) {
         this.userRepository = userRepository;
         this.officeService = officeService;
+        this.authoritiesRepository = authoritiesRepository;
     }
 
     public List<User> findAll() {
@@ -39,16 +45,38 @@ public class UserService {
         userDTO.setId(id);
         userDTO.setUsername(user.getUsername());
         userDTO.setRole(user.getRole());
-        userDTO.setOffice(user.getOffice().getName());
+        userDTO.setOffice(user.getRole().equals("client") ? "": user.getOffice().getName());
 
         return userDTO;
     }
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 
     public void updateUser(UserDTO userDTO) {
-        User user = userRepository.findById(userDTO.getId()).orElse(null);
+
+        User user = userDTO.getId() == null ? new User(): userRepository.findById(userDTO.getId()).orElse(null);
+        List<Authority> auths = authoritiesRepository.findByUsername(user);
         user.setUsername(userDTO.getUsername());
-        user.setRole(userDTO.getRole());
-        user.setOffice(officeService.getOffice(userDTO.getOffice()));
-        userRepository.save(user);
+        user.setRole(Objects.equals(userDTO.getRole(),"") ? "client" : userDTO.getRole());
+        if (Objects.equals(userDTO.getRole(), "employee") && ( userDTO.getOffice() == null || userDTO.getOffice().isEmpty())) {
+            userDTO.setOffice("Default");
+        }
+        user.setOffice(Objects.equals(userDTO.getOffice(), "") ? null :officeService.getOffice(userDTO.getOffice()));
+        if(userDTO.getPassword() != null || !userDTO.getPassword().isEmpty()) {
+            user.setPassword("{noop}"+userDTO.getPassword());
+        }
+
+
+        if(!auths.isEmpty()) {
+            for (Authority auth : auths) {
+                auth.setUsername(user);
+            }
+            authoritiesRepository.saveAll(auths);
+
+        }
+        //userRepository.save(user);
+        Authority auth = new Authority(userDTO.getRole().equals("employee") ? "ROLE_ADMIN" : "ROLE_CLIENT", user);
+        authoritiesRepository.save(auth);
     }
 }
